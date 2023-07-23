3dac38ac290d5975d4c33b7c4b7b49315cd5d791
Shakeeb Parwez <shakeebparwez@gmail.com>
2023-07-24 02:35:58 +0530

    Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import './App.css'
+import { AllRoutes } from "./routes/AllRoutes";
+import { Header } from "./components";
+import './App.css';
 
 function App() {
   return (
-    <>
-
-    </>
-  )
+    <div className="App">
+      <Header />
+      <AllRoutes />
+    </div>
+  );
 }
 
 export default App;

2dfcd6e1e420a268dc57bd9fbbd653edb8e6d3ca
Shakeeb Parwez <shakeebparwez@gmail.com>
2023-07-24 11:19:22 +0530

    Logo update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { Link, NavLink } from "react-router-dom";
-import Logo from "../assets/logo.png"
+import Logo from "../assets/logo2.png"
 import "./Header.css";
 
 export const Header = () => {
